347d18304744a86c821234c8b8ce45531f3c0282
Jon Kinney <jonkinney@gmail.com>
2016-09-12 20:09:08 -0500

    Put back to test

--- app/controllers/preorder_controller.rb
+++ app/controllers/preorder_controller.rb
@@ -10,16 +10,27 @@ class PreorderController < ApplicationController
   def prefill
     @user = User.find_or_create_by(email: params[:email])
 
+    if Settings.use_payment_options
+      payment_option_id = params['payment_option']
+      raise Exception, 'No payment option was selected' if payment_option_id.nil?
+      payment_option = PaymentOption.find(payment_option_id)
+      price = payment_option.amount
+    else
+      price = Settings.price
+    end
+
+    @order = Order.prefill!(name: Settings.product_name, price: price, user_id: @user.id, payment_option: payment_option)
+
+    # This is where all the magic happens. We create a multi-use token with Amazon, letting us charge the user's Amazon account
+    # Then, if they confirm the payment, Amazon POSTs us their shipping details and phone number
+    # From there, we save it, and voila, we got ourselves a preorder!
+    port = Rails.env.production? ? '' : ':3000'
     callback_url = "#{request.scheme}://#{request.host}#{port}/preorder/postfill"
-
-    processed_order =
-      OrderProcessor.new(
-        user: @user,
-        callback_url: callback_url,
-        payment_option_id: params['payment_option']
-      ).process
-
-    redirect_to processed_order.redirect_url
+    redirect_to AmazonFlexPay.multi_use_pipeline(@order.uuid, callback_url,
+                                                 transaction_amount: price,
+                                                 global_amount_limit: price + Settings.charge_limit,
+                                                 collect_shipping_address: 'True',
+                                                 payment_reason: Settings.payment_description)
   end
 
   def postfill
